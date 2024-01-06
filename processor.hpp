301cbfccf9b4a6204b10870a2a825585d0779583
Piotr Wydrych <2383611+wydrych@users.noreply.github.com>
2024-01-06 23:23:07 +0100

    decode advertisements

--- processor.hpp
+++ processor.hpp
@@ -2,6 +2,7 @@
 
 #include <M5Unified.h>
 #include <NimBLEDevice.h>
+#include <mbedtls/ccm.h>
 
 #include "custom_beacon.h"
 
@@ -12,6 +13,15 @@ private:
     {
         uint8_t v[16];
     };
+
+    /* Encrypted atc/custom nonce */
+    /* from from pvvx/ATC_MiThermometer/src/custom_beacon.c */
+    struct __attribute__((packed)) enc_beacon_nonce_t
+    {
+        uint8_t MAC[6];
+        adv_cust_head_t head;
+    };
+
     const std::map<uint64_t, key_t> devices;
 
     static std::map<uint64_t, key_t> convert_devices(std::map<const char *, const char *> devices)
@@ -46,34 +56,105 @@ private:
                head->UUID == ADV_CUSTOM_UUID16;
     }
 
-    static bool decode_adv_cust_enc_t(const padv_cust_enc_t payload)
+    static void dump(const char *n, const uint8_t *d, size_t l)
+    {
+        char hex[l * 2 + 1];
+        for (size_t i = 0; i < l; i++)
+        {
+            sprintf(hex + 2 * i, "%02x", d[i]);
+        }
+        hex[l * 2] = 0;
+        M5_LOGD("%s=(%d)%s", n, l, hex);
+    }
+
+    template <typename T_in, typename T_out>
+    static bool decrypt(T_out *target, const T_in *adv, /*const padv_cust_head_t head, */ const uint64_t &mac, const uint8_t *key /*, const uint8_t *mic*/)
+    {
+        enc_beacon_nonce_t cbn;
+
+        // reverse MAC address
+        cbn.MAC[0] = (uint8_t)(mac >> 0);
+        cbn.MAC[1] = (uint8_t)(mac >> 8);
+        cbn.MAC[2] = (uint8_t)(mac >> 16);
+        cbn.MAC[3] = (uint8_t)(mac >> 24);
+        cbn.MAC[4] = (uint8_t)(mac >> 32);
+        cbn.MAC[5] = (uint8_t)(mac >> 40);
+
+        memcpy(&cbn.head, &adv->head, sizeof(cbn.head));
+
+        uint8_t aad = 0x11;
+
+        mbedtls_ccm_context ctx;
+        mbedtls_ccm_init(&ctx);
+
+        dump("key", key, 16);
+        int ret = mbedtls_ccm_setkey(&ctx, MBEDTLS_CIPHER_ID_AES, key, 16 * 8);
+        if (ret)
+        {
+            M5_LOGE("mbedtls_ccm_setkey() failed: ret = %d", ret);
+            mbedtls_ccm_free(&ctx);
+            return false;
+        }
+
+        // dump("cbn", (uint8_t *)&cbn, sizeof(cbn));
+        // dump("aad", &aad, sizeof(aad));
+        // dump("data", (uint8_t *)&adv->data, sizeof(*target));
+        // dump("mic", adv->mic, 4);
+
+        ret = mbedtls_ccm_auth_decrypt(&ctx, sizeof(adv->data),
+                                       (uint8_t *)&cbn, sizeof(cbn),
+                                       &aad, sizeof(aad),
+                                       (uint8_t *)&adv->data, (uint8_t *)target,
+                                       adv->mic, 4);
+        if (ret)
+        {
+            M5_LOGE("mbedtls_ccm_auth_decrypt() failed: ret = %d", ret);
+            mbedtls_ccm_free(&ctx);
+            return false;
+        }
+
+        mbedtls_ccm_free(&ctx);
+        return true;
+    }
+
+    static bool decode_adv_cust_enc_t(const padv_cust_enc_t payload, const uint64_t &mac, const uint8_t *key)
     {
         if (!verify_adv_cust_head_t(&payload->head))
             return false;
 
         M5_LOGD("decoding custom pvvx encrypted format");
+
+        adv_cust_data_t d;
+        decrypt(&d, payload, mac, key);
+        M5_LOGD("temp %d humi %d bat %d trg %d", d.temp, d.humi, d.bat, d.trg);
+
         return true;
     }
 
-    static bool decode_adv_atc_enc_t(const padv_atc_enc_t payload)
+    static bool decode_adv_atc_enc_t(const padv_atc_enc_t payload, uint64_t mac, const uint8_t *key)
     {
         if (!verify_adv_cust_head_t(&payload->head))
             return false;
 
         M5_LOGD("decoding atc1441 encrypted format");
+
+        adv_atc_data_t d;
+        decrypt(&d, payload, mac, key);
+        M5_LOGD("temp %d humi %d bat %d", d.temp, d.humi, d.bat);
+
         return true;
     }
 
     static bool decode_adv_custom_t(const padv_custom_t payload)
     {
-        M5_LOGD("decoding custom pvvx format");
-        return true;
+        M5_LOGD("decoding custom pvvx format, not implemented yet");
+        return false;
     }
 
     static bool decode_adv_atc1441_t(const padv_atc1441_t payload)
     {
-        M5_LOGD("decoding atc1441 format");
-        return true;
+        M5_LOGD("decoding atc1441 format, not implemented yet");
+        return false;
     }
 
 public:
@@ -104,10 +185,10 @@ public:
         switch (payloadLength)
         {
         case sizeof(adv_cust_enc_t):
-            success = decode_adv_cust_enc_t((padv_cust_enc_t)payload);
+            success = decode_adv_cust_enc_t((padv_cust_enc_t)payload, mac, entry->second.v);
             break;
         case sizeof(adv_atc_enc_t):
-            success = decode_adv_atc_enc_t((padv_atc_enc_t)payload);
+            success = decode_adv_atc_enc_t((padv_atc_enc_t)payload, mac, entry->second.v);
             break;
         case sizeof(adv_custom_t):
             success = decode_adv_custom_t((padv_custom_t)payload);
